f2ab8a95adfe75a308a2b634aa156acc02d31b49
T.Kishimoto <retkhigh009@gmail.com>
2017-10-16 00:31:05 +0900

    Set sample code

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,6 +4,6 @@ import codecheck.mypackage.MyUtil;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(MyUtil.getImageInfluence(args[0], "1", "....../images/sample.img"));
+		System.out.println(MyUtil.getImageInfluence(args[0], "1", "images/sample.jpg"));
 	}
 }
