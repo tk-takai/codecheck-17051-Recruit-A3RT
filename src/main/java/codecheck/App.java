07e635b21fc2f944cbe9d51a5b61936ccd6ddc62
T.Kishimoto <retkhigh009@gmail.com>
2017-10-15 22:59:18 +0900

    Setup sample code

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,6 +4,6 @@ import codecheck.mypackage.MyUtil;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(MyUtil.getImageInfluence(API_KEY_INFLUENCE, "1", "...../images/sample.img"));
+		System.out.println(MyUtil.getImageInfluence(args[0], "1", "...../images/sample.img"));
 	}
 }
--- test/basic.js
+++ test/basic.js
@@ -5,9 +5,9 @@ const app = codecheck.consoleApp(process.env.APP_COMMAND);
 describe("CLIアプリケーション", () => {
 
   it("は実行時に、エラーを発生しない。", () => {
-    return app.codecheck().then( result => {
+    return app.codecheck(API_KEY_INFLUENCE).then(result => {
       expect(result.code).to.equal(0,
-        　"CLIアプリケーションはステータスコード'0'で終了すべきです。");
+        "CLIアプリケーションはステータスコード'0'で終了すべきです。");
     });
   });
 });
