abfb4ba5c3c208259246564bfceb8bf35adf842e
T.Kishimoto <retkhigh009@gmail.com>
2017-10-16 05:18:10 +0900

    Modify utility

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -4,7 +4,7 @@ import codecheck.mypackage.MyUtil;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.println(
-				MyUtil.getImageInfluence(System.getenv("API_KEY_INFLUENCE"), "/root/codecheck/images/sample.jpg"));
+		System.out.println(MyUtil.getImageInfluence(System.getenv("API_KEY_INFLUENCE").toString(), "1",
+				"/root/codecheck/images/sample.jpg"));
 	}
 }
