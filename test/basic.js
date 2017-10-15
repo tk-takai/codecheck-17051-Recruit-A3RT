a648a61adc28e8aeccfb8d8ff5d5a7865a763388
T.Kishimoto <retkhigh009@gmail.com>
2017-10-16 02:46:05 +0900

    Modify js file

--- test/basic.js
+++ test/basic.js
@@ -5,7 +5,7 @@ const app = codecheck.consoleApp(process.env.APP_COMMAND);
 describe("CLIアプリケーション", () => {
 
   it("は実行時に、エラーを発生しない。", () => {
-    return app.codecheck(API_KEY_INFLUENCE).then(result => {
+    return app.codecheck().then(result => {
       expect(result.code).to.equal(0,
         "CLIアプリケーションはステータスコード'0'で終了すべきです。");
     });
